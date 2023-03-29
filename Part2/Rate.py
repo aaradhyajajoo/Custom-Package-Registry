3af9d4553f236ae5e9f5d0e0f4b288db57433d2c
tanvik <tanvikalaskar11@gmail.com>
2023-03-28 21:20:51 -0400

    Calculate code review metric using github URL

--- Part2/Rate.py
+++ Part2/Rate.py
@@ -40,8 +40,38 @@ def calculate_dependency_metric(package_file):
                     num_pinned_deps += 1
         return float(num_pinned_deps) / num_deps
 
-def get_package_url(package_name):
-    """Get the URL of a package on GitHub"""
-    # Replace this with your own logic for obtaining the URL
-    return f'https://github.com/{package_name}'
+def get_github_url(package_id): #Need to find a way to get github URL , this only works if its on PYPI 
+    """Get the GitHub URL of a package given its ID"""
+    url = f'https://pypi.org/pypi/{package_id}/json'
+    response = requests.get(url)
+    if response.status_code != 200:
+        return None
+    data = response.json()
+    info = data['info']
+    if 'project_urls' in info:
+        project_urls = info['project_urls']
+        if 'Source' in project_urls:
+            source_url = project_urls['Source']
+            if 'github.com' in source_url:
+                return source_url
+    return None
+
+def calculate_reviewed_code_fraction(package_github_url):
+    """Calculate the fraction of project code introduced through pull requests with code reviews"""
+    g = Github()
+    repo = g.get_repo(github_url.split('github.com/')[1].strip('/'))
+    pull_request_commits = set()
+    for pr in repo.get_pulls(state='closed'):
+        if pr.merged:
+            for commit in pr.get_commits():
+                pull_request_commits.add(commit.sha)
+    total_lines = 0
+    reviewed_lines = 0
+    for commit in repo.get_commits():
+        if commit.sha in pull_request_commits:
+            for file in commit.files:
+                reviewed_lines += file.changes
+        for file in commit.files:
+            total_lines += file.changes
+    return float(reviewed_lines) / total_lines if total_lines > 0 else 0.0
 
