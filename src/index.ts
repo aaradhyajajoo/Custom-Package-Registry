d8d684c59315fb55ce08dbd8d7dd5f2b25692bac
Tanvi Kalaskar <89361188+tanvikalaskar@users.noreply.github.com>
2023-04-09 19:37:07 -0400

    Delete src directory

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-var json = require('../package.json'); // Package json as a json object
-var dependencies = json.dependencies; // Dependencies as a json object
-var count = Object.keys(dependencies).length; // Number of dependencies
-console.log(count)
