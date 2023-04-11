3b3c006ce09907b5299c7b0e833b8527c6aa03ef
tanvik <tanvikalaskar11@gmail.com>
2023-04-11 16:55:16 -0400

    Rating changed

--- Part2/Rate.py
+++ Part2/Rate.py
@@ -2,34 +2,41 @@ import zipfile
 import re
 import os
 from github import Github
+import tempfile
+import zipfile
+import requests
 
 
-def get_package_name(package_file):
-    """Extract the package name from a zipped package file"""
-    with zipfile.ZipFile(package_file, 'r') as z:
-        for filename in z.namelist():
-            if filename.endswith('setup.py'):
-                with z.open(filename) as f:
-                    setup_content = f.read().decode('utf-8')
-                    match = re.search(
-                        r'name\s*=\s*[\'"]([^\'"]+)[\'"]', setup_content)
-                    if match:
-                        return match.group(1)
-    return None
+def calculate_dependency_metric_from_id(package_id):
+    # Retrieve package metadata
+    response = requests.get(f'https://pypi.org/pypi/{package_id}/json/')
+    if response.status_code != 200:
+        return 1.0
+    metadata = response.json()
+
+    # Extract package name and version requirements
+    name = metadata['info']['name']
+    if 'requires_dist' not in metadata['info']:
+        return 1.0
+    requirements = metadata['info']['requires_dist']
+
+    # Download and extract package files
+    download_url = metadata['urls'][0]['url']
+    with tempfile.TemporaryDirectory() as tmpdir:
+        package_zip_file = os.path.join(tmpdir, f'{name}.zip')
+        with open(package_zip_file, 'wb') as f:
+            response = requests.get(download_url)
+            f.write(response.content)
+        with zipfile.ZipFile(package_zip_file, 'r') as zip_ref:
+            zip_ref.extractall(tmpdir)
+
+        # Calculate dependency metric
+        requirements_file = os.path.join(tmpdir, name, 'requirements.txt')
+        return calculate_dependency_metric(requirements_file, requirements)
 
 
-def unzip_package(file_path, target_dir):
-    """Unzip a package to the target directory"""
-    with zipfile.ZipFile(file_path, 'r') as zip_ref:
-        zip_ref.extractall(target_dir)
-
-
-def calculate_dependency_metric(package_file):
-    """Calculate the fraction of dependencies
-      that are pinned to a specific major+minor version"""
-    target_dir = 'temp_unzip_dir'
-    unzip_package(package_file, target_dir)
-    requirements_file = os.path.join(target_dir, 'requirements.txt')
+def calculate_dependency_metric(requirements_file):
+    """Calculate the fraction of dependencies that are pinned to a specific major+minor version"""
     if not os.path.exists(requirements_file):
         return 1.0
     with open(requirements_file, 'r') as f:
@@ -39,13 +46,12 @@ def calculate_dependency_metric(package_file):
         num_deps = len(lines)
         num_pinned_deps = 0
         for line in lines:
-            match = re.search(r'==(\d+\.\d+)', line)
+            match = re.search(r'==([0-9]+\.[0-9]+)', line)
             if match:
                 version = match.group(1)
                 if version.startswith('2.3.'):
                     num_pinned_deps += 1
         return float(num_pinned_deps) / num_deps
-
 
 # Need to find a way to get github URL , this only works if its on PYPI
 def get_github_url(package_id):
@@ -83,4 +89,5 @@ def calculate_reviewed_code_fraction(github_url):
         for file in commit.files:
             total_lines += file.changes
     return float(reviewed_lines) / total_lines if total_lines > 0 else 0.0
-#hi
+
+
