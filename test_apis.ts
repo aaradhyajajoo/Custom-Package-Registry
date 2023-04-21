f128e9779aa806c44bf9e23b4edc8b8931bce6a3
ibenschi <ibenschi@purdue.edu>
2023-04-20 20:20:40 -0400

    check autho

--- test_apis.ts
+++ test_apis.ts
@@ -8,3 +8,13 @@ test('package API', () => {
   const response = JSON.parse(output || '');
   expect(response).toEqual(expectedJson);
 });
+
+test('check authorization', () => {
+  const curlCommand =  "curl -H 'Content-Type: application/json' --location 'http://127.0.0.1:8080/package'  --data '{\"metadata\":{\"Name\":\"Loadash\",\"Version\":\"1.0.0\",\"ID\":\"loadash\"},\"data\":{\"Content\":\"checking_content\",\"JSProgram\":\"if (process.argv.length === 7) {\\nconsole.log('Success')\\nprocess.exit(0)\\n} else {\\nconsole.log('Failed')\\nprocess.exit(1)\\n}\\n\"}}'";
+  const process = spawnSync(curlCommand, { shell: true });
+  const output = process.stdout?.toString();
+  const expectedJson = { message: 'Success.' }; //????????
+  const response = JSON.parse(output || '');
+  expect(response).toEqual(expectedJson);
+});
+
